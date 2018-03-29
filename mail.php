11372002fd21e5628c8dd943589cd716e80c33d8
mrtarasyuk <tara-boy@mail.ru>
2018-03-30 02:08:06 +0300

    Add

--- mail.php
+++ mail.php
@@ -8,10 +8,9 @@ if ( $method === 'POST' ) {
 	$name = trim($_POST["name"]);
 	$rolli = trim($_POST["rolli"]);
 	$tel  = trim($_POST["tel"]);
-	$form_subject = trim($_POST["form_subject"]);
 
 	foreach ( $_POST as $key => $value ) {
-		if ( $value != "" && $key != "rolli" && $key != "tel" && $key != "form_subject"  && $key != "name" ) {
+		if ( $value != "" && $key != "rolli" && $key != "tel" &&  && $key != "name" ) {
 			$message .= "
 			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
 			<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
